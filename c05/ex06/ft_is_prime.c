5d6eb93f23d8e9112a369eacc1adf024cdb400d4
Kadivv <kadircankaraca01@gmail.com>
2022-11-13 10:45:28 +0300

    Ecole

---
+++ c05/ex06/ft_is_prime.c
@@ -0,0 +1,24 @@
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= (nb / 2))
+	{
+		if (!(nb % i))
+			return (0);
+		else
+			i++;
+	}
+	return (1);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%d", ft_is_prime(19));
+}
+*/
